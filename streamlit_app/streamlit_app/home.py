e5366bcfe91f9f0e9f318f16a02c45d26d3435ea
Eddan <edvin.runhellen1@gmail.com>
2025-06-03 14:58:49 +0200

    Added download links so it works after deploy

--- streamlit_app/streamlit_app/home.py
+++ streamlit_app/streamlit_app/home.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import requests
+from io import BytesIO
+import streamlit as st
 
 # Sidhuvud
 st.title("💎 Välkommen till DiamondPicker")
@@ -28,7 +30,20 @@ st.header("📂 Datasetet: diamonds.csv")
 st.markdown("""
 Du kan ladda ner datasetet nedan och använda det""")
 
-with open("data/diamonds.csv", "rb") as f:
-    st.download_button("⬇️ Ladda ner diamonds.csv", f, file_name="diamonds.csv", mime="text/csv")
+
+# URL till råfilen på GitHub (OBS! Använd råfil, inte visnings-URL)
+url = "https://raw.githubusercontent.com/edvinrunhellen/DiamondLens/v2/streamlit_app/streamlit_app/data/diamonds.csv"
+
+# Hämta innehållet
+response = requests.get(url)
+csv_bytes = BytesIO(response.content)
+
+# Skapa nedladdningsknapp
+st.download_button(
+    label="⬇️ Ladda ner diamonds.csv",
+    data=csv_bytes,
+    file_name="diamonds.csv",
+    mime="text/csv"
+)
 
 st.markdown("""eller ladda upp ditt egna dataset under fliken verktyg""")
