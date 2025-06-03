59d98be17ef0e29bc9185a6872a023c95a623c13
Eddan <edvin.runhellen1@gmail.com>
2025-06-03 14:40:15 +0200

    Added streamlit app to work folder

--- streamlit_app/streamlit_app/pages/3_dataset.py
+++ streamlit_app/streamlit_app/pages/3_dataset.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.header("Översikt över dataset: diamonds.csv")
+st.header("Översikt över dataset: /data/diamonds.csv")
+
+st.write("Notera att datan inte är hanterad efter datakvalite")
+
 df = pd.read_csv("data/diamonds.csv")
 st.write(df.head(100))
